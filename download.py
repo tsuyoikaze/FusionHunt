34861f702373f979c4eb3b441ce3f5af09d03e9b
Martin Qianli Ma <qianlima@brandeis.edu>
2019-01-27 11:24:47 -0500

    download updated

--- download.py
+++ download.py
@@ -1,5 +1,8 @@
 import os
-os.system("prefetch -v [SRRNUMBER]")
-os.system("fastq-dump --outdir /pylon5/mc5frap/siweixu/project --split-files ~/ncbi/public/sra/[SRRNUMBER].sra")
-os.system("rm -rf ncbi")
-os.system("chmod g+w *.fastq")
+f = open("download_srr_series.txt)
+files = map(str.strip, f.readlines())
+for file in files:
+    os.system("prefetch -v %s", file)
+    os.system("fastq-dump --outdir /pylon5/mc5frap/siweixu/project --split-files ~/ncbi/public/sra/%s", file + ".sra")
+    os.system("rm -rf ncbi")
+    os.system("chmod g+w *.fastq")
