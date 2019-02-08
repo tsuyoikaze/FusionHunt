d755ecf7b8a058d287bf5a1e029cfa0a3b79ba1a
Sarah Hsu <36413977+sarahhsu@users.noreply.github.com>
2019-02-08 13:04:47 -0500

    Add more comments for future use

--- rsap.sh
+++ rsap.sh
@@ -4,3 +4,4 @@ perl R-SAP --in1 $1 --in2 $2 --outDir rsap_files --rf GTF --tNum 4
 
 #1 Reference genome alignment file (sorted on RNA‐Seq IDs) of the RNA‐Seq reads in psl format.
 #2 Reference transcript annotation file (UCSC table browser, BED or GTF format). 
+#We are looking for rsap_files/ChimericTranscriptAnnotation.out
