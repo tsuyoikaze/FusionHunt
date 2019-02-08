7a6ff7f8e9dc539a366f0e99aa21e51152173484
siwei xu <siweixu@br018.pvt.bridges.psc.edu>
2019-02-07 21:24:10 -0500

    load_module updated and software installed

--- load_module.sh
+++ load_module.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/bash
 
 # This script adds the required modules into the PSC system
-# This is not required for final submission. Only for PSC
+# This is not required for final submission. Only for PSC machines!
 
+# Manually add Bioperl to perl library and ActivePerl
+export PATH=/pylon5/mc5frap/siweixu/project/lib/ActivePerl-5.26/bin:$PATH
+
+# Create alias for softwares
+alias GFusion="perl /pylon5/mc5frap/siweixu/project/lib/GFusion/GFusion.pl"
+alias gmap="/pylon5/mc5frap/siweixu/project/lib/gmap/bin/gmap"
+alias RepeatMasker="/pylon5/mc5frap/siweixu/project/lib/RepeatMasker/RepeatMasker"
+alias rmblastn="/pylon5/mc5frap/siweixu/project/lib/rmblast/bin/rmblastn"
+alias "R-SAP"="/pylon5/mc5frap/siweixu/project/lib/r-sap-v1.1/R-SAP"
+alias trf="/pylon5/mc5frap/siweixu/project/lib/trf-v4.09/trf"
+
+# PSC built-in software
 module load trinity/2.8.4
 module load tophat/2.1.1
 module load bowtie/1.1.2
