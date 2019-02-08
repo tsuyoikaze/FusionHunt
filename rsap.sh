1115f6c1160eaa42b547b0d0776eb1dedbe772c7
Sarah Hsu <36413977+sarahhsu@users.noreply.github.com>
2019-02-08 13:03:28 -0500

    Update rsap.sh

--- rsap.sh
+++ rsap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir rsap_files
-perl R-SAP --in1 $1 --in2 $2 --outDir rsap_files 
+perl R-SAP --in1 $1 --in2 $2 --outDir rsap_files --rf GTF --tNum 4
 
 #1 Reference genome alignment file (sorted on RNA‐Seq IDs) of the RNA‐Seq reads in psl format.
 #2 Reference transcript annotation file (UCSC table browser, BED or GTF format). 
