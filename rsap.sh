625669dd1607c762fb83f870181281055873150d
Sarah Hsu <36413977+sarahhsu@users.noreply.github.com>
2019-02-08 12:59:47 -0500

    Create rsap.sh

---
+++ rsap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir rsap_files
+perl R-SAP --in1 $1 --in2 $2 --outDir rsap_files 
+
+#1 Reference genome alignment file (sorted on RNA‐Seq IDs) of the RNA‐Seq reads in psl format.
+#2 Reference transcript annotation file (UCSC table browser, BED or GTF format). 
