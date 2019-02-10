2cb0b1ca6df23c52b35bb3d80b0ceea754dd9a7d
Sarah Hsu <36413977+sarahhsu@users.noreply.github.com>
2019-02-10 12:12:58 -0500

    Update rsap.sh

--- rsap.sh
+++ rsap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir rsap_files
 gunzip data/genome/Homo_sapiens.GRCh38.95.gtf.gz
-perl R-SAP --in1 $1 --in2 data/genome/Homo_sapiens.GRCh38.95.gtf --outDir rsap_files --rf GTF --tNum 4
+R-SAP --in1 $1 --in2 data/genome/Homo_sapiens.GRCh38.95.gtf --outDir rsap_files --rf GTF --tNum 4
 rm data/genome/Homo_sapiens.GRCh38.95.gtf
 
 #1 Reference genome alignment file (sorted on RNA‐Seq IDs) of the RNA‐Seq reads in psl format.
