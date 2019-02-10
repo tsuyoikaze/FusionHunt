2dced37877c9f9f44aca9aff34293593196d1fb7
Sarah Hsu <36413977+sarahhsu@users.noreply.github.com>
2019-02-10 11:37:11 -0500

    Updates using the genome gtf now

--- rsap.sh
+++ rsap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 mkdir rsap_files
-perl R-SAP --in1 $1 --in2 $2 --outDir rsap_files --rf GTF --tNum 4
+gunzip data/genome/Homo_sapiens.GRCh38.95.gtf.gz
+perl R-SAP --in1 $1 --in2 data/genome/Homo_sapiens.GRCh38.95.gtf --outDir rsap_files --rf GTF --tNum 4
+rm data/genome/Homo_sapiens.GRCh38.95.gtf
 
 #1 Reference genome alignment file (sorted on RNA‐Seq IDs) of the RNA‐Seq reads in psl format.
-#2 Reference transcript annotation file (UCSC table browser, BED or GTF format). 
+#Reference transcript annotation file (UCSC table browser, BED or GTF format). 
 #We are looking for rsap_files/ChimericTranscriptAnnotation.out
