a0f8fb28cf41a03a3197d1d9e1bde9fc8d30e45d
Qianli Ma <qianlima@br018.pvt.bridges.psc.edu>
2019-02-11 10:15:10 -0500

    add tandem repeat finder, repeatmasker, and gmap

--- trf_rm_gmap/trf_rm_gmap.sh
+++ trf_rm_gmap/trf_rm_gmap.sh
@@ -1,33 +1,47 @@
 #!/bin/bash
-# usage: 
+
+# For testing the program, run source trf_rm_gmap.sh genome.fasta test.fasta
+
+# Usage: 
 # First, load modules using the following command: (in your terminal)
 # source project/src/load_module.sh
-# Second, run this file in the format:
-# source YOUR_DIR_TO_THIS_FILE/run.sh YOUR_DIR_TO_GENOME/GENOME_FILE YOUR_DIR_TO_FASTA/FASTA_FILE
-# The result will be in result.psl.
 
-# TODO: the repeat masker hasn't been implemented
+# Second, run this file in the format:
+# source YOUR_DIR_TO_THIS_FILE/trf_rm_gmap.sh YOUR_DIR_TO_GENOME/GENOME_FILE YOUR_DIR_TO_FASTA/FASTA_FILE
+
+# The result will be in result.psl, in the same directory of tf_rm_gmap.sh
 
 # ----------------------------------------------------------
 GENOME_FILE=$1
 FASTA_FILE=$2
 printf "\n\nUsing genome file: $GENOME_FILE, fasta file: $FASTA_FILE\n\n"
 printf "\n\nLet's run the tests --------------------------------------\n\n"
-source ./project/src/load_module.sh
 
+# Tandem Repeat Finder
+printf "\n\n----------------Tandem Repeat Finder starts---------------\n\n"
 trf $FASTA_FILE 2 7 7 80 10 50 500 -f -d -m
 rm *.html
 rm *.dat
-
 printf "\n\nTandem Repeat Find for $FASTA_FILE Complete.\n\n"
 
+# Repeat Masker
+printf "\n\n----------------Repeat Masker starts---------------\n\n"
 EXTENSION=".2.7.7.80.10.50.500.mask"
 FASTA_FILE="$FASTA_FILE$EXTENSION"
 RepeatMasker -pa 10000 -q -a $FASTA_FILE
-printf "\n\nInterspersed Repeat Find for $FASTA_FILE Complete.\n\n"
 rm -r *RM_*
-
-
-gmap -g $GENOME_FILE -f 1 $FASTA_FILE > result.psl
 rm *.mask
+rm *.cat
+rm *.tbl
+rm *.align
+rm *.out
+rm -r *output*
+printf "\n\nInterspersed Repeat Find (Repeat Masker) for $FASTA_FILE Complete.\n\n"
+
+
+# GMAP
+printf "\n\n----------------GMAP starts---------------\n\n"
+FASTA_FILE="$FASTA_FILE.masked"
+gmap -g $GENOME_FILE -f 1 $FASTA_FILE > result.psl
+rm *.masked
 printf "\n\nGMAP finishes. The result is in result.psl.\n\n"
