3342fd0b8a8ccdb6548e053fab88f32d4bc6500c
Siwei Xu <tuyoikaze@gmail.com>
2019-02-13 15:42:06 -0500

    modifying rsap.sh to accommodate custom file/gtf

--- rsap.sh
+++ rsap.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-mkdir project/rsap_files
-gunzip project/data/genome/Homo_sapiens.GRCh38.95.gtf.gz
-R-SAP --in1 $1 --in2 project/data/genome/Homo_sapiens.GRCh38.95.gtf --outDir project/rsap_files --rf GTF --tNum 4
-rm project/data/genome/Homo_sapiens.GRCh38.95.gtf
+echo "Running: mkdir $3/rsap_files"
+mkdir "$3/rsap_files"
+
+FLAG=false
+FILENAME=$2
+EMPTY_STR=""
+
+if [ ${file: -3} == ".gz" ]; then
+  echo "GZIP file detected; unzipping..."
+  gunzip "$2"
+  FLAG=true
+  FILENAME="${2/.gz/$EMPTY_STR}"
+fi
+
+R-SAP --in1 $1 --in2 $FILENAME --outDir "$3/rsap_files" --rf GTF --tNum 4
+
+if [ $FLAG == true ]; then
+  echo "Re-zipping GZIP file..."
+  gzip "$FILENAME"
 
 #1 Reference genome alignment file (sorted on RNA‐Seq IDs) of the RNA‐Seq reads in psl format.
 #Reference transcript annotation file (UCSC table browser, BED or GTF format). 
