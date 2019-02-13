8a527a90e48215869a2841715ceb327b94b6f7b7
Martin Qianli Ma <qianlima@brandeis.edu>
2019-02-13 17:45:30 -0500

    Delete download.py

--- download.py
+++
@@ -1,8 +0,0 @@
-import os
-f = open("download_srr_series.txt")
-files = map(str.strip, f.readlines())
-for file in files:
-    os.system("prefetch -v %s", file)
-    os.system("fastq-dump --outdir /pylon5/mc5frap/siweixu/project --split-files ~/ncbi/public/sra/%s", file + ".sra")
-    os.system("rm -rf ~/ncbi")
-os.system("chmod g+w *.fastq")
